7d173e7292e4f104588d967e53aa1fd9cad90014
Luis Torgo <ltorgo@gmail.com>
2016-09-06 23:32:49 +0100

    Changes in parameter naming of kNN
    moved references of normalization into standardization, and changed the
    names of the parameters accordingly

--- R/kNN.R
+++ R/kNN.R
@@ -2,16 +2,16 @@
 # This function provides a formula interface to the function
 # knn() in package class, that implements k-nearest neighbours
 # classifiers.
-# On top of that function this adds the possibility of normalizing
-# the data through parameter norm (see examples below)
+# On top of that function this adds the possibility of standardizing
+# the data through parameter stand (see examples below)
 # -----------------------------------------------------------------
 # Mar 2010, Jan 2016, Luis Torgo
 # -----------------------------------------------------------------
-kNN <- function(form, train, test, norm=TRUE, norm.stats=NULL,...) {
+kNN <- function(form, train, test, stand=TRUE, stand.stats=NULL,...) {
   tgtCol <- which(colnames(train)==as.character(form[[2]]))
-  if (norm) {
-    if (is.null(norm.stats)) tmp <- scale(train[,-tgtCol],center=T,scale=T)
-    else tmp <- scale(train[,-tgtCol],center=norm.stats[[1]],scale=norm.stats[[2]])
+  if (stand) {
+    if (is.null(stand.stats)) tmp <- scale(train[,-tgtCol],center=T,scale=T)
+    else tmp <- scale(train[,-tgtCol],center=stand.stats[[1]],scale=stand.stats[[2]])
     train[,-tgtCol] <- tmp
     ms <- attr(tmp,"scaled:center")
     ss <- attr(tmp,"scaled:scale")
