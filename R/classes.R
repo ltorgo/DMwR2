e8c183dd9f3b30ecd048e3e95f4336dfc82c7df6
Luis Torgo <ltorgo@gmail.com>
2016-09-08 23:01:10 +0100

    Preparing from CRAN submission

--- R/classes.R
+++ R/classes.R
@@ -57,15 +57,19 @@ tradeRecord <- function(t,p,tc,c,pf,pp) {
 # --------------------------------------------------------------
 # plot
 #
-.Eq <- function(p) p[,'Equity']
-.St <- function(p) p[,'N.Stocks']
-.addEq <- quantmod::newTA(FUN = .Eq, col = 'red', legend = "Equity")
-.addSt <- quantmod::newTA(FUN = .St, col = 'green', legend = "N.Stocks")
+#.Eq <- function(p) p[,'Equity']
+#.St <- function(p) p[,'N.Stocks']
+#.addEq <- quantmod::newTA(FUN = .Eq, col = 'red', legend = "Equity")
+#.addSt <- quantmod::newTA(FUN = .St, col = 'green', legend = "N.Stocks")
 #.addEq <- quantmod::newTA(FUN = function(p) p[,'Equity'], col = 'red', legend = "Equity")
 #.addSt <- quantmod::newTA(FUN = function(p) p[,'N.Stocks'], col = 'green', legend = "N.Stocks")
 
 setMethod("plot", "tradeRecord",
   function(x,y,verbose=TRUE,...) {
+      .Eq <- function(p) p[,'Equity']
+      .St <- function(p) p[,'N.Stocks']
+      .addEq <- quantmod::newTA(FUN = .Eq, col = 'red', legend = "Equity")
+      .addSt <- quantmod::newTA(FUN = .St, col = 'green', legend = "N.Stocks")
       tr <- x
       market <- cbind(y,zoo::coredata(x@trading)[,c('Equity','N.Stocks')])
       quantmod::candleChart(market,
